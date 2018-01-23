38f915f517909b3f3ba4ce11dbd1c1b6470b67bc
Hleb Maliborski <gleb.maliborsky@gmail.com>
2018-01-23 17:15:18 +0300

    Test commit

--- app/src/main/java/hmaliborski/com/circleview/view/CircleView.java
+++ app/src/main/java/hmaliborski/com/circleview/view/CircleView.java
@@ -5,7 +5,10 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.support.annotation.Nullable;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -14,11 +17,19 @@ import hmaliborski.com.circleview.R;
 public class CircleView extends View {
     private static final String TAG = CircleView.class.getSimpleName();
 
-    private int mCircleColor;
-    private boolean isBorder;
-    private int mBorderColor;
+    private String mTitleText;
 
-    private Paint mPaint;
+    private int mCircleColor;
+    private int mLoadingColor;
+    private int borderColor;
+    private int mTitleColor;
+    private float mBorderWidth;
+    private float mTitleSize;
+
+    private TextPaint mTextPaint;
+    private Paint mInnerPaint;
+    private Paint mOuterPaint;
+    private RectF mRectF;
 
     public CircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -28,24 +39,42 @@ public class CircleView extends View {
     private void init(Context context, AttributeSet set) {
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(set, R.styleable.CircleView, 0, 0);
 
+
         try {
+            mTitleColor = typedArray.getInt(R.styleable.CircleView_titleColor, Color.BLACK);
+            mLoadingColor = typedArray.getInt(R.styleable.CircleView_loadingColor, Color.BLACK);
             mCircleColor = typedArray.getInt(R.styleable.CircleView_circleColor, Color.BLACK);
-            isBorder = typedArray.getBoolean(R.styleable.CircleView_isBorder, false);
-            mBorderColor = typedArray.getColor(R.styleable.CircleView_borderColor, Color.BLACK);
+            borderColor = typedArray.getColor(R.styleable.CircleView_borderColor, Color.BLACK);
+            mBorderWidth = typedArray.getFloat(R.styleable.CircleView_borderWidth, 1);
+            mTitleSize = typedArray.getFloat(R.styleable.CircleView_titleSize, 18);
+            mTitleText = typedArray.getString(R.styleable.CircleView_titleText);
         } finally {
             typedArray.recycle();
         }
 
-        mPaint = new Paint();
-        mPaint.setColor(mCircleColor);
-        mPaint.set
-        mPaint.setAntiAlias(true);
+        mRectF = new RectF();
+
+        mInnerPaint = new Paint();
+        mInnerPaint.setAntiAlias(true);
+        mInnerPaint.setColor(mCircleColor);
+
+        mOuterPaint = new Paint();
+        mOuterPaint.setAntiAlias(true);
+        mOuterPaint.setStyle(Paint.Style.STROKE);
+        mOuterPaint.setStrokeWidth(mBorderWidth);
+
+        mTextPaint = new TextPaint();
+        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+        mTextPaint.setLinearText(true);
+        mTextPaint.setColor(mTitleColor);
+        mTextPaint.setTextSize(mTitleSize);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         int width = getWidth();
         int height = getHeight();
 
@@ -57,12 +86,22 @@ public class CircleView extends View {
         int actualWidth = width - (leftPadding + rightPadding);
         int actualHeight = height - (topPadding + bottomPadding);
 
-        int radius = Math.min(actualWidth, actualHeight) / 2;
+        float radius = Math.min(actualWidth, actualHeight) / 2;
 
         int cx = leftPadding + actualWidth / 2;
         int cy = topPadding + actualHeight / 2;
 
-        canvas.drawCircle(cx, cy, radius, mPaint);
+        canvas.drawCircle(cx, cy, radius, mInnerPaint);
+
+        mOuterPaint.setColor(borderColor);
+        radius = radius - mBorderWidth / 2;
+        canvas.drawCircle(cx, cy, radius, mOuterPaint);
+
+        mRectF.set(cx - radius, cy - radius, cx + radius, cy + radius);
+        mOuterPaint.setColor(mLoadingColor);
+
+        canvas.drawArc(mRectF, 0, 30, false, mOuterPaint);
+        canvas.drawText(mTitleText, cx, cy, mTextPaint);
     }
 
     @Override
